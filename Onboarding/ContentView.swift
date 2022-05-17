89dea815d7dbff6b5bd8217f6b7efc4cc72a0ce7
Kha Tran <khatran961217@gmail.com>
2022-05-17 10:54:25 +1000

    Initial Commit

---
+++ Onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding
+//
+//  Created by Kha Tran on 17/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Kha Tran on 17/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
